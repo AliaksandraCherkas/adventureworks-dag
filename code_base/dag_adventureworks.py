f949874f2be856748190adbb5ad7e9d925d719ca
Aliaksandra Cherkas <aliaksandra_cherkas@epam.com>
2025-08-08 10:55:33 +0600

    modify dag

--- code_base/dag_adventureworks.py
+++ code_base/dag_adventureworks.py
@@ -43,7 +43,7 @@ JOB_1_SQL_TO_PARQUET = {
     "reference": {"project_id": GCP_PROJECT_ID},
     "placement": {"cluster_name": CLUSTER_NAME},
     "pyspark_job": {
-        "main_python_file_uri": f"gs://{GCS_BUCKET}/scripts/code_base/db_to_parquet2.py",
+        "main_python_file_uri": f"gs://{GCS_BUCKET}/scripts/code_base/db_to_parquet.py",
         "jar_file_uris": [f"gs://{GCS_BUCKET}/scripts/jars/postgresql-42.7.7.jar"],
     },
 }
@@ -52,7 +52,7 @@ JOB_2_PARQUET_TO_BQ = {
     "reference": {"project_id": GCP_PROJECT_ID},
     "placement": {"cluster_name": CLUSTER_NAME},
     "pyspark_job": {
-        "main_python_file_uri": f"gs://{GCS_BUCKET}/scripts/code_base/transform_to_bq1.py",
+        "main_python_file_uri": f"gs://{GCS_BUCKET}/scripts/code_base/transform_to_bq.py",
     },
 }
 
